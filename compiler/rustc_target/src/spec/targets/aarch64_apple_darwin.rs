6606ebd70fe6f84eaea81abf28e7d0954dcf4037
jhilton <jhilton@mit.edu>
2024-04-20 14:20:28 -0400

    Load libopencilk when linking

--- compiler/rustc_target/src/spec/targets/aarch64_apple_darwin.rs
+++ compiler/rustc_target/src/spec/targets/aarch64_apple_darwin.rs
@@ -1,5 +1,5 @@
 use crate::spec::base::apple::{macos_llvm_target, opts, Arch};
-use crate::spec::{FramePointer, SanitizerSet, Target, TargetOptions};
+use crate::spec::{Cc, FramePointer, LinkerFlavor, Lld, SanitizerSet, Target, TargetOptions};
 
 pub fn target() -> Target {
     let arch = Arch::Arm64;
@@ -10,6 +10,14 @@ pub fn target() -> Target {
     // FIXME: The leak sanitizer currently fails the tests, see #88132.
     base.supported_sanitizers = SanitizerSet::ADDRESS | SanitizerSet::CFI | SanitizerSet::THREAD;
 
+    let mut late_link_args = std::collections::BTreeMap::new();
+    // FIXME(jhilton): this path should reference something inside the build directory at some point
+    let args_to_link_opencilk: Vec<std::borrow::Cow<'static, str>> = vec![
+        "-L".into(),
+        "/Users/jay/Code/MEng/opencilk/build/lib/clang/17/lib/darwin".into(),
+        "-lopencilk_osx_dynamic".into(),
+    ];
+    late_link_args.insert(LinkerFlavor::Darwin(Cc::Yes, Lld::No), args_to_link_opencilk.clone());
     Target {
         // Clang automatically chooses a more specific target based on
         // MACOSX_DEPLOYMENT_TARGET. To enable cross-language LTO to work
@@ -21,6 +29,7 @@ pub fn target() -> Target {
         options: TargetOptions {
             mcount: "\u{1}mcount".into(),
             frame_pointer: FramePointer::NonLeaf,
+            late_link_args,
             ..base
         },
     }
